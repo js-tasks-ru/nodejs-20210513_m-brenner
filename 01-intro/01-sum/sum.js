cf6aa7a083316dfe5f5127d867c022f1c198dcab
Mariya Brenner <m.brenner@kazdream.kz>
2021-05-17 21:11:03 +0600

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-  /* ваш код */
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('Ошибка! Аргументы не являются числами!')
+    }
+    return a + b;
 }
 
 module.exports = sum;
